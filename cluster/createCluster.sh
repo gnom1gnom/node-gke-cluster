62f80cb02e5ac445a0718baf9856046e6d01fc79
Krzysztof Jurkowski <krzysztof.jurkowski@roche.com>
2022-11-09 16:21:07 +0000

    COS_CONTAINERD

--- cluster/createCluster.sh
+++ cluster/createCluster.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-gcloud beta container --project "diacovidbot" clusters create "dev-cluster" \
+gcloud container --project "diacovidbot" clusters create "dev-cluster" \
     --zone "us-central1-c" \
     --release-channel "regular" \
     --machine-type "e2-small" \
-    --image-type "COS" \
+    --image-type "COS_CONTAINERD" \
     --disk-type "pd-ssd" \
     --disk-size "10" \
     --enable-ip-alias \
     --preemptible --num-nodes "3" \
-    --enable-autoscaling --min-nodes "3" --max-nodes "9" \
+    --enable-autoscaling --min-nodes "3" --max-nodes "5" \
     --addons HorizontalPodAutoscaling,HttpLoadBalancing,NodeLocalDNS \
     --enable-autoupgrade \
     --enable-autorepair \
